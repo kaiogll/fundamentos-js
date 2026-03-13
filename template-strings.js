16c657b1daec313f7bcdae2d48a9eec2d8d8d6c9
kaiogll <kaio.laureano@escola.pr.gov.br>
2026-03-13 12:03:14 -0300

    template final

---
+++ template-strings.js
@@ -0,0 +1,10 @@
+const nome = 
+const idade = 2026-1981;
+const cidadeDeNascimento = "Pitanga"; 
+
+//const apresentacao = "Meu nome é " + ", minha idade é" + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = ' Meu nome é $ {nome}  minha 
+idade é ${idade} e nasci na cidade de $
+{cidadeDeNacimento}';
+console.log(apresentacao):
