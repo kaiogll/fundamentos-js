27996e204933407fdec92c4510f8e9b61322d66f
kaiogll <kaio.laureano@escola.pr.gov.br>
2026-06-02 13:34:20 +0000

    refatoração

--- Arrays/procurando-aluno.js
+++ Arrays/procurando-aluno.js
@@ -1,14 +1,19 @@
 const alunos = ["João" , "Juliana" , "Ana" , "Caio"];
 
-const mediasDeAlunosEMedias = [10 , 8, 7.5 , 9];
+const medias = [10 , 8, 7.5 , 9];
 
 const listaDeAlunosEMedias = [alunos, medias];
 
 function exibeNomeENota(aluno){
     if(listaDeAlunosEMedias[0].includes(aluno)){
-       console.log(`${aluno} está cadastrado!`);
+        const [alunos, medias] = listaDeAlunosEMedias;
+
+        const indice = alunos.indexOf(aluno);
+
+        const mediaDoAluno = medias[indice];
+       console.log(`${aluno} tem a média ${mediaDoAluno}`);
     } else {
-        console.log(`${aluno} não está cadastrado.`);
+        console.log(`Aluno não cadastrado!`);
     }
 }
 
