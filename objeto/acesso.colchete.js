bdf1e4f6d32be86e961e472051fce108b563fbf3
kaiogll <kaio.laureano@escola.pr.gov.br>
2026-08-14 08:35:24 -0300

    .

---
+++ objeto/acesso.colchete.js
@@ -0,0 +1,16 @@
+const cliente = {
+    nome: "kaio",
+    idade: 44,
+    cpf "1122233345",
+    email: "kaio@dominio.com"
+};
+
+console.log ('O nome do cliente é $ {cliente["nome"]'} e essa pessoa tem ${cliente["idade"]} anos.'');
+
+const chaves = ["nome" , "idade" , "cpf" , "email"];
+
+chaves.console.log.forEach(element => {
+    console.log(' A chave ${chave} tem valor &')
+});
+
+    
